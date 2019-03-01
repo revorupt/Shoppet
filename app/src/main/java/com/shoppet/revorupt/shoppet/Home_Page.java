4bed200a3ecb1a9839047d2917a1bfc0c23ac61a
macoh.musada <cristhianmacoh.musada@gmail.com>
2019-03-02 00:23:38 +0800

    Initial commit

---
+++ app/src/main/java/com/shoppet/revorupt/shoppet/Home_Page.java
@@ -0,0 +1,13 @@
+package com.shoppet.revorupt.shoppet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__page);
+    }
+}
